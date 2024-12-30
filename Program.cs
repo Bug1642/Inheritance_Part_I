1bf4719b476e8f9eeb07650943adaaf533a94644
Bug1642 <hasancan1642@hotmail.com>
2024-12-30 14:51:13 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise
+{
+    public class Animal
+    {
+        public virtual string GiveBirth()
+        {
+            return "I don’t know how";
+        }
+    }
+    public class Mammal : Animal
+    {
+        public override string GiveBirth()
+        {
+            return "I give live birth";
+        }
+    }
+    public class Bird : Animal
+    {
+        public override string GiveBirth()
+        {
+            return "I lay eggs";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Console.WriteLine(animal.GiveBirth());
+
+            Mammal mammal = new Mammal();
+            Console.WriteLine(mammal.GiveBirth());
+
+            Bird bird = new Bird();
+            Console.WriteLine(bird.GiveBirth());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Inheritance_Part_I
+ Inheritance Part I
